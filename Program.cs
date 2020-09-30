ddbaa83b93d95b9fd036902c0647c0240eb9da5a
unknown <bogdan.stana97@e-uvt.ro>
2020-09-30 22:54:38 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Diagnostics;
+
+namespace ProcessesList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Process[] processes = Process.GetProcesses();
+
+            foreach (Process process in processes)
+            {
+                try
+                {
+                    Console.WriteLine(process.ProcessName + " / " + process.MainModule.FileName + "\t" +
+                        process.Id + "\t" +
+                        (process.PagedSystemMemorySize64 / 1024));
+                }
+                catch (System.ComponentModel.Win32Exception ex)
+                {
+                    Console.WriteLine(process.ProcessName);
+                }
+            }
+        }
+    }
+}
